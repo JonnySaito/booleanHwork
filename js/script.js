b3a93f88cb4f371ab316dc653b2a24fb2abe0095
John Bretherton <270022735@bun2-206-02.local>
2019-05-06 15:33:22 +1200

    add boolean

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+console.log("js is working");
+
+// $(function(){
+//   // add jQuery methods here; if click on rightCircle when nav open, then rightCircle background colour goes to red;
+//   //                           if click on rightCircle when nav closed, then rightCircle bg colour goes blue.
+//   // $("navbar-toggler-icon").on({
+//   //   click: function(){
+//   //     $(rightCircle).css("background-color", "red");
+//   //   }
+//   // })
+// });
